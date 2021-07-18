e0cf6b23a1ea8ab7d9a7a8a4907202f9c3e2b216
Lutovit <pilot_mig@bk.ru>
2021-07-18 21:30:16 +0300

    Добавьте файлы проекта.

---
+++ PhoneStore/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PhoneStore.Models;
+
+namespace PhoneStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        AppDbContext db;
+
+        public HomeController(AppDbContext context)
+        {
+            db = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(db.Phones.ToList());
+        }
+
+
+        [HttpGet]
+        public IActionResult Buy(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.PhoneId = id;
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Buy(Order order)
+        {
+            db.Orders.Add(order);
+
+            db.SaveChanges();
+
+            return View("Thanks");
+        }
+
+
+    }
+}
---
+++ PhoneStore/Models/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhoneStore.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Phone> Phones { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+
+    }
+}
